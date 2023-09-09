34ff401817a880706e97955b0110ed0a64354184
Sankarapu Samhith <samhiths17@gmail.com>
2023-09-09 15:34:29 +0530

    config

---
+++ components/config.py
@@ -0,0 +1,9 @@
+
+import os
+from dataclasses import dataclass
+
+@dataclass
+class IngestionConfig:
+    train_data_path: str=os.path.join('artifacts',"train.csv")
+    test_data_path: str=os.path.join('artifacts',"test.csv")
+    raw_data_path: str=os.path.join('artifacts',"data.csv")
