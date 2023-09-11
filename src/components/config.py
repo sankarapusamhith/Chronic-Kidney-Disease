c57e347dc04dab322265e2d15117bd20da7ba620
Sankarapu Samhith <samhiths17@gmail.com>
2023-09-12 00:21:31 +0530

    Transform Config

--- src/components/config.py
+++ src/components/config.py
@@ -7,3 +7,8 @@ class IngestionConfig:
     train_data_path: str=os.path.join('artifacts',"train.csv")
     test_data_path: str=os.path.join('artifacts',"test.csv")
     raw_data_path: str=os.path.join('artifacts',"data.csv")
+
+@dataclass
+class TransformationConfig:
+    preprocessor_obj_file_path=os.path.join('artifacts',"proprocessor.pkl")
+
