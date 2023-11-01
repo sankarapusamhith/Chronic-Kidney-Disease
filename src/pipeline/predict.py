35425aa39ee4b23f2e55bb911e66ca268808065b
Sankarapu Samhith <samhiths17@gmail.com>
2023-11-01 11:16:46 +0530

    prediction pipeline

---
+++ src/pipeline/predict.py
@@ -0,0 +1,24 @@
+import sys
+import os
+from src.exception import CustomException
+from src.utils import load_object
+
+
+class PredictPipeline:
+    def __init__(self):
+        pass
+
+    def predict(self,features):
+        try:
+            model_path=os.path.join("artifacts","model.pkl")
+            preprocessor_path=os.path.join('artifacts','preprocessor.pkl')
+            print("Before Loading")
+            model=load_object(file_path=model_path)
+            preprocessor=load_object(file_path=preprocessor_path)
+            print("After Loading")
+            data_scaled=preprocessor.transform(features)
+            preds=model.predict(data_scaled)
+            return preds
+        
+        except Exception as e:
+            raise CustomException(e,sys)
